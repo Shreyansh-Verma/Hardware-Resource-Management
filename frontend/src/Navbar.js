ef3c7406b73f6957e9a092f3219c6a20d150bc84
AryanDubey2389 <77682366+AryanDubey2389@users.noreply.github.com>
2023-12-05 02:49:19 +0530

    Update Navbar.js

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import profile from './img/anime3.png'
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar(props) {
+  
     const [isUserMenuOpen, setUserMenuOpen] = useState(false);
     const [isMainMenuOpen, setMainMenuOpen] = useState(false);
+    
+    const active = props?.currentTab ? props?.currentTab : 'Home';
+    const [activeTab, setActiveTab] = useState(active);
+    const handleTabClick = (tab) => {
+      setActiveTab(tab);
+    };
     const toggleUserMenu = () => {
         setUserMenuOpen(!isUserMenuOpen);
         setMainMenuOpen(false);
@@ -14,8 +22,8 @@ function Navbar() {
         setUserMenuOpen(false);
       };
     return (
-    <div>
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <div className='bg-indigo-950'>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 bg-indigo-950">
     <div className="max-w-screen-xl flex flex-wrap  justify-between mx-auto  p-4">
     <div className="flex items-center">
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Code Till Cope</span>
@@ -29,57 +37,39 @@ function Navbar() {
         <span className="sr-only">Open user menu</span>
         <img className="w-8 h-8 rounded-full" src={profile} alt="user photo" />
       </button>
-      {isUserMenuOpen && (
-        <div className="mr-4 absolute right-0 top-12 z-50 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-          <div className="px-4 py-3">
-            <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-            <span className="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-          </div>
-          <ul>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover-bg-gray-600 hover:text-blue-500 dark:text-gray-200 ">
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover-bg-gray-600 hover:text-blue-500 dark:text-gray-200 ">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover-bg-gray-600 hover:text-blue-500 dark:text-gray-200 ">
-                Earnings
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover-bg-gray-600 hover:text-blue-500 dark:text-gray-200 ">
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
 
     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+          <Link
+            to="/"
+            onClick={() => handleTabClick('Home')}
+            className={`block py-2 pl-3 pr-4 rounded ${
+              activeTab === 'Home'
+                ? 'text-blue-700'
+                : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+            }`}
+            aria-current={activeTab === 'Home' ? 'page' : null}
+          >
+            Home
+          </Link>
         </li>
         <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+          <Link
+            to="/dashboard"
+            onClick={() => handleTabClick('About')}
+            className={`block py-2 pl-3 pr-4 rounded ${
+              activeTab === 'Dashboard'
+                ? 'text-blue-700'
+                : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+            }`}
+          >
+            Dashboard
+          </Link>
         </li>
-        <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-        </li>
-        <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Dashboard</a>
-        </li>
-        <li>
-            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-        </li>
-        </ul>
-    </div>
+      </ul>
+    </div>  
     </div>
     </nav>
     </div>
